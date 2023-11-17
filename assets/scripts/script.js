8818b536a2cf745dcfaf82820c0ef94ee2443e01
Alvjo Dauti <adauti21@epoka.edu.al>
2023-11-17 08:46:06 -1200

    pop up js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -68,3 +68,131 @@ function togglePassword()
 };
 
 //end show password
+
+//make cart page button active for pop up
+
+class Order{
+    constructor(_id,_CarModel, _CarBrand, _Price){
+  
+        if(arguments.length != 4)
+        {
+            throw new Error("Please, provide 4 properties")
+        }
+        this.id= _id;
+        this.CarModel = _CarModel;
+        this.CarBrand = _CarBrand;
+        this.Price = _Price;
+        
+    }
+  }
+  
+  const orders = [
+    new Order( 1,'M5 E60 V10','BMW ','$40,000'),
+    new Order(2,'911 TURBO S', 'PORSCHE','$180,345'),
+    new Order(3,'','','Total = $220,345')
+  ];
+  
+  //Get Table -> <tbody>
+  const ordersTableBody = $(".cart1 tbody");
+  ordersTableBody.empty();
+  
+  function populateTable(){
+    $.each(orders, function(index, order){
+  
+        console.log(`Index = ${index}. Order = ${order}`);
+  
+        if(index==2){
+          const newRowHtml = `<tr>
+            <td>${order.CarModel}</td>
+            <td>${order.CarBrand}</td>
+            <td>${order.Price}</td>
+            <td>
+                 <button id="confirmBtn" data-order-id="${order.id}">Continue</button>
+            </td>
+        </tr>`;
+        ordersTableBody.append(newRowHtml);
+        }
+        else{
+          const newRowHtml = `<tr>
+          <td>${order.CarModel}</td>
+          <td>${order.CarBrand}</td>
+          <td>${order.Price}</td>
+          <td>
+              <button id="removeBtn" data-order-id="${order.id}">Remove</button>
+          </td>
+      </tr>`;
+      ordersTableBody.append(newRowHtml);
+        }
+        
+  
+        
+    });
+  }
+  
+  populateTable();
+  
+  // $(ordersTableBody).on('click', "#editBtn", function(){
+  //   const orderId = $(this).data('order-id');
+  //   const order = orders.find(n => n.id === orderId);
+  //   // editOrder(order);
+  
+  //   console.log($`Selected order = ${order}`);
+  
+  //   $("#CarModel").val(order.CarModel);
+  //   $("#CarBrand").val(order.CarBrand);
+  //   $("#Price").val(order.Price);
+  
+  //   $("#editModal").show();
+  // })
+  
+  
+  $(ordersTableBody).on('click', "#removeBtn", function(){
+    const orderId = $(this).data('order-id');
+    const order = orders.find(n => n.id === orderId);
+    // editOrder(order);
+  
+    console.log($`Selected order = ${order}`);
+    $("#remove-CarBrand").text(order.CarBrand);
+    $("#remove-CarModel").text(order.CarModel);
+    $("#remove-Price").text(order.Price);
+  
+    $("#removeModal").show();
+  })
+  
+  $("#conBtn").click(function(){
+    window.alert("Kur e pe ne fillim te pelqeu , tani ste pelqeu aaa!");
+    $("#removeModal").hide();
+  
+  });
+  
+  $("#cancelRemoveBtn").click(function(){
+    $("#removeModal").hide();
+  
+  });
+  
+  
+  
+  
+  $(ordersTableBody).on('click', "#confirmBtn", function(){
+    const orderId = $(this).data('order-id');
+    const order = orders.find(n => n.id === orderId);
+    // editOrder(order);
+  
+    //console.log($`Selected order = ${order}`);
+    $("#continueModal").show();
+  });
+  
+  
+  
+  $("#noBtn").click(function(){
+    $("#continueModal").hide();
+  })
+  
+  $("#yesBtn").click(function(){
+  
+    $("#continueModal").hide();
+    
+  alert("Thank you for choosing us !" );
+});
+  
+  //end cart pop up
